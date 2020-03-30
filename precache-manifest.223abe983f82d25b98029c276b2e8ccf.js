ab06fd056138100a8175d3ee389c7b81658260fd
TuzDmitry <dimatuz94@gmail.com>
2020-03-30 19:54:30 +0300

    Updates

---
+++ precache-manifest.223abe983f82d25b98029c276b2e8ccf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce28ad526840391c51e2dc1f1f9fead",
+    "url": "/04-todo/index.html"
+  },
+  {
+    "revision": "ea75692e74220762a2e4",
+    "url": "/04-todo/static/css/main.094d1ee1.chunk.css"
+  },
+  {
+    "revision": "4903575d6ca7fe1f5d07",
+    "url": "/04-todo/static/js/2.5d2946b9.chunk.js"
+  },
+  {
+    "revision": "ea75692e74220762a2e4",
+    "url": "/04-todo/static/js/main.15cd54fe.chunk.js"
+  },
+  {
+    "revision": "111e6cae2656dfd92c0d",
+    "url": "/04-todo/static/js/runtime~main.c0e2e360.js"
+  }
+]);
